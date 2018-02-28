f46291a43ccbf4d8e3172b442f482f0b999f3597
Agent-Fitz <Agent-Fitz@users.noreply.github.com>
2018-02-28 09:56:32 -0600

    Update test3.jl

--- test3.jl
+++ test3.jl
@@ -1,7 +1,7 @@
 function RainyDays(days) # input: number of days in a year
     x = rand(days)
     rainy = zero(days)
-    for i in 1:days:1 # iterator is broken! :( 
+    for i in 1:days # iterator is broken! :( 
         x[i] > 0.500 && (rainy += 1)
     end
     return rainy
